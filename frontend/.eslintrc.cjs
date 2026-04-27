8cfb95dfeef3d31f865ae08edf4e42d234b17641
owine <owine@users.noreply.github.com>
2026-04-27 08:41:32 -0500

    build: scaffold frontend (vite + react + ts + eslint)

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2022: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules', '.tsbuild'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 2022, sourceType: 'module' },
+  plugins: ['@typescript-eslint', 'react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+  },
+};
